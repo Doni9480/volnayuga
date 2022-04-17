55cfb8c07e42e9bf5cdc7690ecd4d7a98bff0ad3
justscoundrel <justscoundrel@yandex.ru>
2022-04-17 22:11:16 +0300

    Add whitenoise3

--- VolnaYuga/settings.py
+++ VolnaYuga/settings.py
@@ -207,3 +207,19 @@ CKEDITOR_CONFIGS = {
 
 YANDEX_MAPS_API_KEY=env('YANDEX_MAPS_API_KEY')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
