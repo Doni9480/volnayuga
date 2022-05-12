bae3c249b0e277505d5563a6f25bf980ce3222bf
justscoundrel <justscoundrel@yandex.ru>
2022-05-12 17:45:03 +0300

    Add filter type of objects2

--- hotel/models.py
+++ hotel/models.py
@@ -12,7 +12,7 @@ from region.models import Region
 class TypeofObject(models.Model):
     """Типы объектов сдачи"""
     title = models.CharField(max_length=100, verbose_name='Название')
-    slug = models.SlugField(default='default', verbose_name='URL')
+    slug = models.SlugField(unique=True, verbose_name='URL')
     image = models.ImageField(upload_to='hotel/hoteltype', null=True, blank=True, verbose_name='Изображение')
 
     class Meta:
