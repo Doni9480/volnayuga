1b79cdd8758c0e4abcb4527968403ad2fe529dbd
justscoundrel <justscoundrel@yandex.ru>
2022-10-18 22:53:24 +0300

    Add region in sitemap

--- core/sitemaps.py
+++ core/sitemaps.py
@@ -1,12 +1,13 @@
 from django.contrib.sitemaps import Sitemap
 
 from hotel.models import Hotel
+from region.models import Region
 
 
 class HotelSitemap(Sitemap):
     changefreq = "weekly"
     priority = 0.8
-    protocol = 'http'
+    protocol = 'https'
 
     def items(self):
         return Hotel.objects.all()
@@ -16,3 +17,15 @@ class HotelSitemap(Sitemap):
 
     def location(self, obj):
         return '/%s/%s' % (obj.city.slug, obj.id)
+
+
+class RegionSitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.8
+    protocol = 'https'
+
+    def items(self):
+        return Region.objects.all()
+
+    def location(self, obj):
+        return '/%s' % (obj.slug)
