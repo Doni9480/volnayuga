e6dea6076e3028964291221eee11aa2371089610
Андрей Галкин <justscoundrel@yandex.ru>
2022-12-06 01:56:34 +0300

    Добавил куроррты рядом исключения на страниц5

--- hotel/templatetags/hotel_tags.py
+++ hotel/templatetags/hotel_tags.py
@@ -1,5 +1,5 @@
 from django import template
-
+from collections import Counter
 from hotel.models import TypeofObject, Hotel
 
 register = template.Library()
@@ -12,10 +12,12 @@ def sort_by(queryset, order):
 @register.simple_tag()
 def get_type_of_object(value):
     hotel_list = Hotel.objects.filter(city=value)
-    type_of_object_list = {}
+    type_of_object_list = []
     for item in hotel_list:
         for object_type in item.object_type.all():
-            if object_type not in type_of_object_list:
-                type_of_object_list[object_type] = object_type
-                type_of_object_list[object_type] = object_type.hotel_set.all().count()
-    return type_of_object_list
+            type_of_object_list.append(object_type)
+    counter = {}
+    for elem in type_of_object_list:
+        counter[elem] = counter.get(elem, 0) + 1
+    doubles = {element: count for element, count in counter.items() if count > 1}
+    return counter
