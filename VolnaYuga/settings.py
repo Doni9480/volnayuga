0b8433306db1804a70aa2a293f2c6bdd09b769b4
doni <daniilmirlanuulu@gmail.com>
2023-06-18 22:32:52 +0600

    Made sending requests for feedback by e-mail

--- VolnaYuga/settings.py
+++ VolnaYuga/settings.py
@@ -190,13 +190,16 @@ django_heroku.settings(locals())
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_HOST_USER = "daniilmirlanuulu@gmail.com"
-EMAIL_HOST_PASSWORD = "pfvvkbjhopqylgrw"
-EMAIL_USE_TLS=True
-EMAIL_USE_SSL=False
+# Settings sending email
+
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = env('EMAIL_HOST') 
+EMAIL_PORT = env('EMAIL_PORT') 
+EMAIL_HOST_USER = env('EMAIL_HOST_USER') 
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = env('EMAIL_USE_TLS',default=True) 
+EMAIL_USE_SSL = env('EMAIL_USE_SSL',default=False) 
 
 # django-ckeditor
 # https://github.com/django-ckeditor/django-ckeditor
