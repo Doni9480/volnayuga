5ef131c71393337550996032a105a54d62aeff01
Андрей Галкин <justscoundrel@yandex.ru>
2023-07-12 11:35:47 +0300

    Add favorites hotel

--- accounts/models.py
+++ accounts/models.py
@@ -5,13 +5,12 @@ from django.contrib.auth.models import UserManager
 from django.contrib.auth.base_user import BaseUserManager
 
 
-
-
 class CustomUserManager(BaseUserManager):
     """
     Custom user model manager where email is the unique identifiers
     for authentication instead of usernames.
     """
+
     def create_user(self, email, password, **extra_fields):
         """
         Create and save a User with the given email and password.
@@ -54,7 +53,5 @@ class MyUser(AbstractUser):
     REQUIRED_FIELDS = []
     objects = CustomUserManager()
 
-
     def __str__(self):
         return self.email
-
--- hotel/views.py
+++ hotel/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import DetailView, ListView
 
+from accounts.models import MyUser
 from review.models import Review
-from .models import Hotel
+from .models import Hotel, BookmarkHotel
 from region.models import Region
+import json
+
+from django.contrib import auth
+from django.http import HttpResponse, JsonResponse
+from django.views import View
 
 
 def home_view(request):
@@ -11,13 +17,81 @@ def home_view(request):
     review_list = Review.objects.filter(verificated=True)
     region_popular_list = Region.objects.filter(is_popular=True)
     context = {
-        'region_list':region_list,
-        'region_popular_list':region_popular_list,
-        'review_list':review_list,
+        'region_list': region_list,
+        'region_popular_list': region_popular_list,
+        'review_list': review_list,
     }
     return render(request, template_name='hotel/home.html', context=context)
 
 
+class BookmarkView(View):
+    # в данную переменную будет устанавливаться модель закладок, которую необходимо обработать
+    model = BookmarkHotel
+
+    def post(self, request, pk):
+        # when the user is unauthenticated, before calling a method that takes request as a parameter do:
+        if request.user.is_anonymous:
+            # нам потребуется пользователь
+            anonUser, created = MyUser.objects.get_or_create(email="anonuser@anon.ru", phone='111111111')
+            print(anonUser, created)
+            user = MyUser.objects.get(id=anonUser.id)
+        else:
+            user = auth.get_user(request)
+        # пытаемся получить закладку из таблицы, или создать новую
+        bookmark, created = self.model.objects.get_or_create(user=user, hotel_id=pk)
+        # если не была создана новая закладка,
+        # то считаем, что запрос был на удаление закладки
+        if not created:
+            bookmark.delete()
+
+        return HttpResponse(
+            json.dumps({
+                "result": created,
+                "count": self.model.objects.filter(hotel_id=pk).count()
+            }),
+            content_type="application/json"
+        )
 
 
+def favorites_list(request):
+    favorites_list_ids = request.session.get('favorites')
+    favorites_list = []
+    for item in favorites_list_ids:
+        favorites_list.append(Hotel.objects.get(id=item))
+    context = {
+        'favorite_list': favorites_list
+    }
+    return render(request, 'hotel/bookmarks.html', context)
 
+
+def add_to_favorites(request, id):
+    if request.is_ajax() and request.method == 'POST':
+        if not request.session.get('favorites'):
+            request.session['favorites'] = list()
+        else:
+            request.session['favorites'] = list(request.session['favorites'])
+
+        if id not in request.session['favorites']:
+            request.session['favorites'].append(id)
+            request.session.modified = True
+            message = 'Добавлено в избранное'
+            return JsonResponse(status=200, data={'status':'true','message':message})
+
+        else:
+            request.session['favorites'].remove(id)
+            request.session.modified = True
+            message = 'Удалено из избранного'
+            return JsonResponse(status=200, data={'status': 'true', 'message': message})
+    return JsonResponse(status=400, data={'status': 'false', 'message': 'Что то пошло не так!'})
+
+
+def remove_from_favorites(request, id):
+    if request.method == 'POST':
+        for item in request.session['favorites']:
+            if item['id'] == id:
+                item.clear()
+    if not request.session['favorites']:
+        del request.session['favorites']
+
+    request.session.modified = True
+    return redirect(request.POST.get('url_form'))
