33ac75724b20833679477637b6035d14697ee154
Daniil <daniilmirlanuulu@gmail.com>
2023-07-18 00:06:30 +0600

    task 4

--- static/script/hotel_detail.js
+++ static/script/hotel_detail.js
@@ -2,7 +2,9 @@ $(document).ready(() => {
     $('.whatsapp-question-handler').on('click', () => {
         const current_url = window.location.href;
         const message = `Здравствуйте! \nМы нашли ваше объявление на ${current_url}`;
-        const link_to_whatsapp = `https://wa.me/+996706305623?text=${message}`;
+        const phone = $('.whatsapp-question-handler').data('phone');
+        console.log(phone);
+        const link_to_whatsapp = `https://wa.me/${phone}?text=${message}`;
         window.open(link_to_whatsapp, "_blank");
     })}
 )
